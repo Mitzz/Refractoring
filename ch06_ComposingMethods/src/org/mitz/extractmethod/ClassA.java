0b4e173bda41971975cfc70fb576e00456f8f99b
mitzCoder <mithul.bhansali80@gmail.com>
2018-06-23 16:17:09 +0530

    ch06_ComposingMethods - 3C - getOutstanding()

--- ch06_ComposingMethods/src/org/mitz/extractmethod/ClassA.java
+++ ch06_ComposingMethods/src/org/mitz/extractmethod/ClassA.java
@@ -9,28 +9,29 @@ public class ClassA {
 	private String _name;
 
 	void printOwing() {
-		Enumeration e = _orders.elements();
-		double outstanding = 0.0;
-		
 		printBanner();
-		
-		// calculate outstanding
-		while (e.hasMoreElements()) {
-			Order each = (Order) e.nextElement();
-			outstanding += each.getAmount();
-		}
-
+		double outstanding = getOutstanding();
 		printDetails(outstanding);
 	}
-	
+
+	private double getOutstanding() {
+		Enumeration e = _orders.elements();
+		double result = 0.0;
+		while (e.hasMoreElements()) {
+			Order each = (Order) e.nextElement();
+			result += each.getAmount();
+		}
+		return result;
+	}
+
 	void printBanner() {
 		System.out.println("**************************");
 		System.out.println("***** Customer Owes ******");
 		System.out.println("**************************");
 	}
-	
+
 	void printDetails(double outstanding) {
-		System.out.println ("name:" + _name);
-		System.out.println ("amount" + outstanding);
+		System.out.println("name:" + _name);
+		System.out.println("amount" + outstanding);
 	}
 }
