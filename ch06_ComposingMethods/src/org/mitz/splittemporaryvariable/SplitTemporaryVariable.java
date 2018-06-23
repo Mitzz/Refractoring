49d62c57f13cb143af0ebd187d8b4efa721fc42c
mitzCoder <mithul.bhansali80@gmail.com>
2018-06-23 17:45:08 +0530

    SplitTemporaryVariable
    Setup

---
+++ ch06_ComposingMethods/src/org/mitz/splittemporaryvariable/SplitTemporaryVariable.java
@@ -0,0 +1,23 @@
+package org.mitz.splittemporaryvariable;
+
+public class SplitTemporaryVariable {
+
+	private int _primaryForce;
+	private int _mass;
+	private int _delay;
+	private int _secondaryForce;
+
+	double getDistanceTravelled(int time) {
+		double result;
+		double acc = _primaryForce / _mass;
+		int primaryTime = Math.min(time, _delay);
+		result = 0.5 * acc * primaryTime * primaryTime;
+		int secondaryTime = time - _delay;
+		if (secondaryTime > 0) {
+			double primaryVel = acc * _delay;
+			acc = (_primaryForce + _secondaryForce) / _mass;
+			result += primaryVel * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+		}
+		return result;
+	}
+}
