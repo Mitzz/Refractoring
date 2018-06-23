6e516ca5f4d31270ef94f9533c44fd91674cc9f3
mitzCoder <mithul.bhansali80@gmail.com>
2018-06-23 16:45:39 +0530

    Replace Temp With Query
    Declaring final

--- ch06_ComposingMethods/src/org/mitz/replacetempwithquery/ReplaceTempWithQuery.java
+++ ch06_ComposingMethods/src/org/mitz/replacetempwithquery/ReplaceTempWithQuery.java
@@ -6,8 +6,8 @@ public class ReplaceTempWithQuery {
 	private int _itemPrice;
 
 	double getPrice() {
-		int basePrice = _quantity * _itemPrice;
-		double discountFactor;
+		final int basePrice = _quantity * _itemPrice;
+		final double discountFactor;
 		if (basePrice > 1000)
 			discountFactor = 0.95;
 		else
