0e74542c099ba7bd3ab2fdb675868e0f288dd6fb
mitzCoder <mithul.bhansali80@gmail.com>
2018-06-23 16:15:04 +0530

    ch06_ComposingMethods - 2C - printDetails()

--- ch06_ComposingMethods/src/org/mitz/extractmethod/ClassA.java
+++ ch06_ComposingMethods/src/org/mitz/extractmethod/ClassA.java
@@ -19,14 +19,18 @@ public class ClassA {
 			Order each = (Order) e.nextElement();
 			outstanding += each.getAmount();
 		}
-		// print details
-		System.out.println("name:" + _name);
-		System.out.println("amount" + outstanding);
+
+		printDetails(outstanding);
 	}
 	
 	void printBanner() {
 		System.out.println("**************************");
 		System.out.println("***** Customer Owes ******");
 		System.out.println("**************************");
+	}
+	
+	void printDetails(double outstanding) {
+		System.out.println ("name:" + _name);
+		System.out.println ("amount" + outstanding);
 	}
 }
