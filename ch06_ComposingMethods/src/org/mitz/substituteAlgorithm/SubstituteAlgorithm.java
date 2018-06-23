1af5c30486eff45ec615bba9dbdad8c5f2da3bdf
mitzCoder <mithul.bhansali80@gmail.com>
2018-06-23 21:55:45 +0530

    SubstituteAlgorithm
    Refractored Code

--- ch06_ComposingMethods/src/org/mitz/substituteAlgorithm/SubstituteAlgorithm.java
+++ ch06_ComposingMethods/src/org/mitz/substituteAlgorithm/SubstituteAlgorithm.java
@@ -1,19 +1,15 @@
 package org.mitz.substituteAlgorithm;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SubstituteAlgorithm {
 
 	String foundPerson(String[] people) {
-		for (int i = 0; i < people.length; i++) {
-			if (people[i].equals("Don")) {
-				return "Don";
-			}
-			if (people[i].equals("John")) {
-				return "John";
-			}
-			if (people[i].equals("Kent")) {
-				return "Kent";
-			}
-		}
+		List candidates = Arrays.asList(new String[] { "Don", "John", "Kent" });
+		for (int i = 0; i < people.length; i++)
+			if (candidates.contains(people[i]))
+				return people[i];
 		return "";
 	}
 }
