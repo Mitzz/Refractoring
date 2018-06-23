48cb73d8db5eb928db4ab988f898247aba49d11c
mitzCoder <mithul.bhansali80@gmail.com>
2018-06-23 17:33:46 +0530

    Introduce Explaining Variable
    Third Local Variable as final

--- ch06_ComposingMethods/src/org/mitz/introducingexplainingvariable/IntroduceExplainingVariable.java
+++ ch06_ComposingMethods/src/org/mitz/introducingexplainingvariable/IntroduceExplainingVariable.java
@@ -6,9 +6,9 @@ public class IntroduceExplainingVariable {
 	private int _itemPrice;
 
 	double price() {
-		// price is base price - quantity discount + shipping
 		final double basePrice = _quantity * _itemPrice;
 		final double quantityDiscount = Math.max(0, _quantity - 500) * _itemPrice * 0.05;
-		return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100.0);
+		final double shipping = Math.min(basePrice * 0.1, 100.0);
+		return basePrice - quantityDiscount + shipping;
 	}
 }
