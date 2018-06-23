41324eca6736ef44c722ff65db613a18fc4ededf
mitzCoder <mithul.bhansali80@gmail.com>
2018-06-23 21:25:21 +0530

    ReplaceMethodWithMethodObjects
    Further Refractoring

--- ch06_ComposingMethods/src/org/mitz/replaceMethodWithMethodObject/Gamma.java
+++ ch06_ComposingMethods/src/org/mitz/replaceMethodWithMethodObject/Gamma.java
@@ -19,10 +19,14 @@ public class Gamma {
 	int compute() {
 		importantValue1 = (inputVal * quantity) + _account.delta();
 		importantValue2 = (inputVal * yearToDate) + 100;
-		if ((yearToDate - importantValue1) > 100)
-			importantValue2 -= 20;
+		importantThing();
 		int importantValue3 = importantValue2 * 7;
 		// and so on.
 		return importantValue3 - 2 * importantValue1;
+	}
+
+	void importantThing() {
+		if ((yearToDate - importantValue1) > 100)
+			importantValue2 -= 20;
 	}
 }
