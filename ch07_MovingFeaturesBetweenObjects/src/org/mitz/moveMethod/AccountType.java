782ed31701a46e32eb3bffd461eb4368459fba2e
mitzCoder <mithul.bhansali80@gmail.com>
2018-06-24 09:25:08 +0530

    MoveMethod
    Move Method by Volatile AccountType

--- ch07_MovingFeaturesBetweenObjects/src/org/mitz/moveMethod/AccountType.java
+++ ch07_MovingFeaturesBetweenObjects/src/org/mitz/moveMethod/AccountType.java
@@ -3,8 +3,16 @@ package org.mitz.moveMethod;
 public class AccountType {
 
 	public boolean isPremium() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+	double overdraftCharge(int daysOverdrawn) {
+		if (isPremium()) {
+			double result = 10;
+			if (daysOverdrawn > 7)
+				result += (daysOverdrawn - 7) * 0.85;
+			return result;
+		} else
+			return daysOverdrawn * 1.75;
+	}
 }
