91ee976079a43f2a17e57212a42d21c3d928203e
Unknown <mithul.bhansali80@gmail.com>
2018-06-30 15:20:59 +0530

    Inline Class
    Setup Code

---
+++ ch07_MovingFeaturesBetweenObjects/src/org/mitz/inlineClass/TelephoneNumber.java
@@ -0,0 +1,22 @@
+package org.mitz.inlineClass;
+
+public class TelephoneNumber {
+	private String _areaCode;
+	private String _number;
+
+	String getAreaCode() {
+		return _areaCode;
+	}
+
+	void setAreaCode(String arg) {
+		_areaCode = arg;
+	}
+
+	String getNumber() {
+		return _number;
+	}
+
+	void setNumber(String arg) {
+		_number = arg;
+	}
+}
