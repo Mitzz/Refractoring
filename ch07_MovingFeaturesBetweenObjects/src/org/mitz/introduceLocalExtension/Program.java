380e6ddc7106d8e5ca0983df2a71cd102220c235
Unknown <mithul.bhansali80@gmail.com>
2018-06-30 19:02:15 +0530

    Introduce Local Extension
    Setup Code

---
+++ ch07_MovingFeaturesBetweenObjects/src/org/mitz/introduceLocalExtension/Program.java
@@ -0,0 +1,16 @@
+package org.mitz.introduceLocalExtension;
+
+import java.util.Date;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Date previousEnd = new Date();
+		Date newStart = nextDay(previousEnd);
+	}
+
+	private static Date nextDay(Date previousEnd) {
+		return new Date(previousEnd.getYear(), previousEnd.getMonth(), previousEnd.getDate() + 1);
+	}
+}
