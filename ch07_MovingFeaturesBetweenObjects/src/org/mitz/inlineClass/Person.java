94d2da44df051325cc7c546dbcd6272397a274d6
Unknown <mithul.bhansali80@gmail.com>
2018-06-30 15:36:59 +0530

    Inline Class
    Declaring all the visible methods on telephone number on person

--- ch07_MovingFeaturesBetweenObjects/src/org/mitz/inlineClass/Person.java
+++ ch07_MovingFeaturesBetweenObjects/src/org/mitz/inlineClass/Person.java
@@ -3,7 +3,7 @@ package org.mitz.inlineClass;
 public class Person {
 	private String _name;
 	private TelephoneNumber _officeTelephone = new TelephoneNumber();
-	
+
 	public String getName() {
 		return _name;
 	}
@@ -25,6 +25,22 @@ public class Person {
 	}
 
 	void setOfficeNumber(String arg) {
+		_officeTelephone.setNumber(arg);
+	}
+
+	String getAreaCode() {
+		return _officeTelephone.getAreaCode();
+	}
+
+	void setAreaCode(String arg) {
+		_officeTelephone.setAreaCode(arg);
+	}
+
+	String getNumber() {
+		return _officeTelephone.getNumber();
+	}
+
+	void setNumber(String arg) {
 		_officeTelephone.setNumber(arg);
 	}
 }
