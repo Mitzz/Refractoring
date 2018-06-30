e140d97d6633760f99ae27861bf7bf6504b022ac
Unknown <mithul.bhansali80@gmail.com>
2018-06-30 14:55:45 +0530

    MoveField
    Getter and Setter of the field

--- ch07_MovingFeaturesBetweenObjects/src/org/mitz/moveField/AccountType.java
+++ ch07_MovingFeaturesBetweenObjects/src/org/mitz/moveField/AccountType.java
@@ -2,4 +2,13 @@ package org.mitz.moveField;
 
 public class AccountType {
 
+	private double _interestRate;
+
+	void setInterestRate(double arg) {
+		_interestRate = arg;
+	}
+
+	double getInterestRate() {
+		return _interestRate;
+	}
 }
