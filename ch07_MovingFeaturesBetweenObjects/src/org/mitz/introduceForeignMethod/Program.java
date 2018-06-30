025b20e1cee5ab8b639aa4a1003297f4db6fb96d
Unknown <mithul.bhansali80@gmail.com>
2018-06-30 18:28:47 +0530

    Introduce Foreign Method
    Refactored Code

--- ch07_MovingFeaturesBetweenObjects/src/org/mitz/introduceForeignMethod/Program.java
+++ ch07_MovingFeaturesBetweenObjects/src/org/mitz/introduceForeignMethod/Program.java
@@ -7,7 +7,12 @@ public class Program {
 	public static void main(String[] args) {
 
 		Date previousEnd = new Date();
+		nextDay(previousEnd);
+	}
+
+	private static void nextDay(Date previousEnd) {
 		Date newStart = new Date(previousEnd.getYear(), previousEnd.getMonth(), previousEnd.getDate() + 1);
 	}
 
+	
 }
