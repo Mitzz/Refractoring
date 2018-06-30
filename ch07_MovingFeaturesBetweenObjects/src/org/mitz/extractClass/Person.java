1a12db554f05e88b69491338f366947b58b4cf3e
Unknown <mithul.bhansali80@gmail.com>
2018-06-30 15:09:33 +0530

    ExtractClass
    Use MoveField refactoring for one instance variable

--- ch07_MovingFeaturesBetweenObjects/src/org/mitz/extractClass/Person.java
+++ ch07_MovingFeaturesBetweenObjects/src/org/mitz/extractClass/Person.java
@@ -2,7 +2,6 @@ package org.mitz.extractClass;
 
 public class Person {
 	private String _name;
-	private String _officeAreaCode;
 	private String _officeNumber;
 	private TelephoneNumber _officeTelephone = new TelephoneNumber();
 	
@@ -11,15 +10,15 @@ public class Person {
 	}
 
 	public String getTelephoneNumber() {
-		return ("(" + _officeAreaCode + ") " + _officeNumber);
+		return ("(" + _officeTelephone.getAreaCode() + ") " + _officeNumber);
 	}
 
 	String getOfficeAreaCode() {
-		return _officeAreaCode;
+		return _officeTelephone.getAreaCode();
 	}
 
 	void setOfficeAreaCode(String arg) {
-		_officeAreaCode = arg;
+		_officeTelephone.setAreaCode(arg);
 	}
 
 	String getOfficeNumber() {
--- ch07_MovingFeaturesBetweenObjects/src/org/mitz/extractClass/TelephoneNumber.java
+++ ch07_MovingFeaturesBetweenObjects/src/org/mitz/extractClass/TelephoneNumber.java
@@ -1,5 +1,14 @@
 package org.mitz.extractClass;
 
 public class TelephoneNumber {
+	private String _areaCode;
+
+	String getAreaCode() {
+		return _areaCode;
+	}
+
+	void setAreaCode(String arg) {
+		_areaCode = arg;
+	}
 
 }
