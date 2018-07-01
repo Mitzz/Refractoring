9f88dc120c213d441099ad1328dda09fe834b0c6
Unknown <mithul.bhansali80@gmail.com>
2018-07-01 19:13:22 +0530

    Change Reference to Value
    Refactored Code

--- ch08_OrganizingData/src/org/mitz/changeReferenceToValue/Currency.java
+++ ch08_OrganizingData/src/org/mitz/changeReferenceToValue/Currency.java
@@ -7,12 +7,39 @@ public class Currency {
 		return _code;
 	}
 
-	private Currency(String code) {
+	public Currency(String code) {
 		_code = code;
 	}
 
 	public static Currency get(String string) {
 		return null;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((_code == null) ? 0 : _code.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Currency other = (Currency) obj;
+		if (_code == null) {
+			if (other._code != null)
+				return false;
+		} else if (!_code.equals(other._code))
+			return false;
+		return true;
+	}
+	
+	
 
 }
--- ch08_OrganizingData/src/org/mitz/changeReferenceToValue/Program.java
+++ ch08_OrganizingData/src/org/mitz/changeReferenceToValue/Program.java
@@ -3,6 +3,7 @@ package org.mitz.changeReferenceToValue;
 public class Program {
 
 	public static void main(String[] args) {
-		Currency usd = Currency.get("USD");
+		Currency usd = new Currency("USD");
+		System.out.println(usd.equals(new Currency("USD")));
 	}
 }
