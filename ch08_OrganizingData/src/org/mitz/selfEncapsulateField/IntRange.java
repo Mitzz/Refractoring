f3cde00e33f79b5ebf80a041cf0b6adc5d14fe4c
mitzCoder <mithul.bhansali80@gmail.com>
2018-07-01 10:22:38 +0530

    Self Encapsulate Field
    Setup Code

---
+++ ch08_OrganizingData/src/org/mitz/selfEncapsulateField/IntRange.java
@@ -0,0 +1,19 @@
+package org.mitz.selfEncapsulateField;
+
+public class IntRange {
+
+	private int _low, _high;
+
+	boolean includes(int arg) {
+		return arg >= _low && arg <= _high;
+	}
+
+	void grow(int factor) {
+		_high = _high * factor;
+	}
+
+	IntRange(int low, int high) {
+		_low = low;
+		_high = high;
+	}
+}
